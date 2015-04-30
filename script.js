9fbf80357e58c766cb5acf0715ab308b6ab9a985
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-04-30 09:38:50 -0500

    Added pirates

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ var bellie = {
 
 var ninjas =[raphael, ninjakitty, bellie]	
 
+var sparrow = {
+	name: "sparrow",
+	image: "http://static.hdw.eweb4.com/media/thumbs/1/104/1030110.jpg"
+}
+
+var spongeB = {
+	name: "spongeB" ,
+	image: "http://stuffpoint.com/spongebob-square-pants/image/76301-spongebob-square-pants-pirate-with-sponge-bob.jpg"
+}
+
+var scaryP = {
+	name: "scaryP" ,
+	image: "http://www.takeourword.com/images/pirate.gif"
+}
+
+var pirates =[sparrow, spongeB, scaryP]
+
+
 var i;
 
 $(document).ready(function(){
@@ -26,6 +44,21 @@ $(document).ready(function(){
 	for (i=0; i<ninjas.length; i++)
 		if(identifier == ninjas[i].name) {
 			$('.col-lg-10').append("<img src=" + ninjas[i].image +">")
+		}
+
+	});
+
+
+		$('.dropdown-menu-left li div').on("click", function(){
+		// console.log("pick me");
+		$('.col-lg-10').children().remove();
+		
+		var identifier = $(this).data("id");
+		// console.log(identifier);
+		
+	for (i=0; i<pirates.length; i++)
+		if(identifier == pirates[i].name) {
+			$('.col-lg-10').append("<img src=" + pirates[i].image +">")
 		}
 
 	});
